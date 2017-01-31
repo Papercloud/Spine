626c1fd7c8f6531a79c8f6826309b375dd8e39ae
Mark Turner <git@markst.co.uk>
2017-01-31 12:27:12 +1100

    Temporary Spine resolution for `URLValueFormatter` not handling optional return value wvteijlingen/Spine#137

--- Spine/ValueFormatter.swift
+++ Spine/ValueFormatter.swift
@@ -134,7 +134,12 @@ struct ValueFormatterRegistry {
 /// it will return an absolute URL, relative to the baseURL.
 private struct URLValueFormatter: ValueFormatter {
 	func unformatValue(_ value: String, forAttribute attribute: URLAttribute) -> URL {
-		return URL(string: value, relativeTo: attribute.baseURL as URL?)!
+        let url = URL(string: value, relativeTo: attribute.baseURL as URL?)
+        if url == nil {
+            return URL(string: "www.example.com")!
+        } else {
+            return url!
+        }
 	}
 	
 	func formatValue(_ value: URL, forAttribute attribute: URLAttribute) -> String {
